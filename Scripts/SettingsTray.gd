d667c08b483fd10a55857b2216bfa90a870de641
Mihai Cioroianu <Mihai.CIOROIANU@edenred.com>
2022-11-24 17:10:44 +0200

    Fixed Settings Tray Collision

--- Scripts/SettingsTray.gd
+++ Scripts/SettingsTray.gd
@@ -12,7 +12,7 @@ var newPosition = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position = Vector2(0, 20)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
