c2435341509d26270faec242277f7331baf098d6
Mihai Cioroianu <Mihai.CIOROIANU@edenred.com>
2022-11-24 17:01:00 +0200

    Added Settings Tray

---
+++ Scripts/SettingsTray.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var maximumMovement = 105
+var minimumMovement = 20
+var movement = Vector2.ZERO
+var speed = 150
+var newPosition = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	newPosition = position+(movement*speed)*delta
+	if newPosition.y>maximumMovement:
+		newPosition.y = maximumMovement
+	elif newPosition.y<minimumMovement:
+		newPosition.y = minimumMovement
+	position = newPosition
+	
+
+
+func _on_Area2D_mouse_entered():
+	movement = Vector2.DOWN
+
+
+func _on_Area2D_mouse_exited():
+	movement = Vector2.UP
