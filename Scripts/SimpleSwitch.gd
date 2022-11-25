62bc1d3e055d204f28341a6c4b68eef192a22694
Mihai Cioroianu <Mihai.CIOROIANU@edenred.com>
2022-11-25 15:19:09 +0200

    Added Simple Setting Switch Item

---
+++ Scripts/SimpleSwitch.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var settingName = ""
+export var label = ""
+signal settingChange(setting, value)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = label
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_pressed(button_pressed):
+	emit_signal("settingChange", settingName, button_pressed)
