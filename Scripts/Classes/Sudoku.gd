e1912ca37a5e59bac62e1fe8a74a3318e36dbf86
Mihai Cioroianu <Mihai.CIOROIANU@edenred.com>
2022-11-29 11:19:29 +0200

    Added Universal Sudoku Object

---
+++ Scripts/Classes/Sudoku.gd
@@ -0,0 +1,18 @@
+extends Node
+
+class_name Sudoku
+
+enum SUDOKU_FORMAT {FORMAT_9X9, FORMAT_6X6, FORMAT_4X4, FORMAT_HEX}
+var id:int
+var sudokuName:String
+var format:int
+var data
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
