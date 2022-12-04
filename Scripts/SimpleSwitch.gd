a78d636d77e594dd8ed73bb8cdf62797814823b9
Mihai Cioroianu <Mihai.CIOROIANU@edenred.com>
2022-12-04 20:06:31 +0200

    Implemented Settings Persistence

--- Scripts/SimpleSwitch.gd
+++ Scripts/SimpleSwitch.gd
@@ -14,11 +14,12 @@ func _ready():
 	$Label.text = label
 	$TextureButton.set_pressed_no_signal(currentValue)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func refresh(settings):
+	$TextureButton.set_pressed_no_signal(settings[settingName])
 
 func _on_pressed(button_pressed):
 	emit_signal("settingChange", settingName, button_pressed)
