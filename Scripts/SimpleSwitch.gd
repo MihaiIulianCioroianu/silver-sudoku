840f3b944a9ddf0e684c5b3bfad91fa51ede5827
Mihai Cioroianu <Mihai.CIOROIANU@edenred.com>
2022-12-04 19:47:30 +0200

    Fixed Setting Tray Simple Switch

--- Scripts/SimpleSwitch.gd
+++ Scripts/SimpleSwitch.gd
@@ -6,11 +6,13 @@ class_name SimpleSwitch
 # var b = "text"
 export var settingName = ""
 export var label = ""
+export var currentValue = true
 signal settingChange(setting, value)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Label.text = label
+	$TextureButton.set_pressed_no_signal(currentValue)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
